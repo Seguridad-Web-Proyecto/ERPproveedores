876e0aa083d2566f70e53048bb9d7caa19d3f0a3
camilo <jcamilovelandiab@gmail.com>
2020-04-23 19:27:44 -0500

    agregando configuraciones para intentar desplegar en un servidor

--- src/main/java/restapplication/HelloRESTController.java
+++ src/main/java/restapplication/HelloRESTController.java
@@ -15,6 +15,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @Path("")
@@ -22,8 +23,8 @@ public class HelloRESTController {
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String hello() {
-        return "Hola!. Esta es la ruta de servicios web!!!";
+    public Response hello() {
+        return Response.ok("Hola!. Esta es la ruta de servicios web!!!").build();
     }
     
 }
